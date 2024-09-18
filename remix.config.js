302249cf152f5924c72739eb9d0af8f12f2b29db
Matthew Sirkis <mattsirkis@Matthews-MBP.localdomain>
2024-09-18 13:47:49 -0800

    set up  tailwind

---
+++ remix.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+    // Ensure this matches your project structure
+    appDirectory: "app",
+    assetsBuildDirectory: "public/build",
+    serverBuildPath: "build/index.js",
+    publicPath: "/build/",
+    // Add any other necessary configuration options
+  };
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+// import postcss from './postcss.config.js';
 
 export default defineConfig({
+  css: {
+    postcss: './postcss.config.js',
+  },
   plugins: [
     remix({
       future: {
